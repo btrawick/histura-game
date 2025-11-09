fc618e72acb11936709b0a40e84d9463cbb1e198
btrawick <btrawick@gmail.com>
2025-11-09 16:47:30 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // use HashRouter if deploying to Pages; fine on Vercel too
+import { BrowserRouter } from 'react-router-dom'; // Vercel supports this (pretty URLs)
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
