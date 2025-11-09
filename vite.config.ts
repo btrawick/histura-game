d3d4799e35caf6712bea8f9df2d49a3bd71d0c4c
btrawick <btrawick@gmail.com>
2025-11-09 16:36:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-//vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/histura-game/' : '/',
-})
+  // Set this if deploying to GitHub Pages; safe to leave as-is on Vercel.
+  base: process.env.GITHUB_PAGES ? '/histura-game/' : '/'
+});
