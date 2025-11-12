3f03b076c8ad921ad007c2eef0d0b7531e788f46
btrawick <btrawick@gmail.com>
2025-11-12 10:24:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { NavLink, Outlet, Route, Routes } from 'react-router-dom'
-import Home from '@/routes/Home'
-import Play from '@/routes/Play'
-import Playback from '@/routes/Playback'
-import Settings from '@/routes/Settings'
-import './styles/globals.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '@/routes/Home';
+import Play from '@/routes/Play';
+import Playback from '@/routes/Playback';
+import Settings from '@/routes/Settings';
+import AppHeader from '@/components/AppHeader';
 
 export default function App() {
   return (
-    <div>
-      <nav className="container">
-        <NavLink to="/" end>Home</NavLink>
-        <NavLink to="/play">Play</NavLink>
-        <NavLink to="/playback">Playback</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/play" element={<Play />} />
-        <Route path="/playback" element={<Playback />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      <Outlet />
-    </div>
-  )
+    <BrowserRouter>
+      <AppHeader />
+      <div style={{ maxWidth: 960, margin: '0 auto', padding: 12 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/playback" element={<Playback />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
